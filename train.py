2bcd55dcda0ce1e1f6f993236816c467c58ef631
shahnisarg <shah.nisar@husky.neu.edu>
2018-05-06 03:57:32 -0400

    Preprocessing the contents

--- train.py
+++ train.py
@@ -1,14 +1,37 @@
 import os
-path = os.getcwd() + os.path.sep + "resources" + os.path.sep + "english_big.txt"
+import string
 
-spam_count = 0
-ham_count = 0
-with open(path, "r") as file:
-    for line in file:
-        line = line.replace("\n", "")
-        tokens = line.split(",")
-        if tokens[-1] == "spam":
-            spam_count += 1
-        else:
-            ham_count += 1
-print spam_count, ham_count
+
+def get_count(path):
+    s_count = 0
+    total_count = 0
+    with open(path, "r") as file:
+        for line in file:
+            line = pre_process(line)
+            label = line.rsplit(" ", 1)[1]
+            content = line.rsplit(' ', 1)[0]
+            process(content)
+            total_count += 1
+            if label == "spam":
+                s_count += 1
+    return s_count, total_count-s_count
+
+
+def pre_process(line):
+    for c in string.punctuation:
+        line = line.replace(c, " ")
+        line = ''.join([i if ord(i) < 123 else '' for i in line])
+    line = line.replace("\n", "")
+    print line
+    return line
+
+
+def process(content):
+    print content # Process the content here
+
+path = os.getcwd() + os.path.sep + "resources" + os.path.sep + "english_big.txt"
+spam_count, ham_count = get_count(path)
+p_spam = spam_count/float(spam_count + ham_count)
+p_ham = ham_count/float(spam_count + ham_count)
+
+print p_spam, p_ham
